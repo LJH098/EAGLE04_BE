f971e95f53b68e987295e03a6a277cb9e3580784
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-27 21:24:55 +0900

    feat: 초기 세팅

---
+++ src/main/java/EAGLE04/demo/Eagle04Application.java
@@ -0,0 +1,13 @@
+package EAGLE04.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eagle04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eagle04Application.class, args);
+	}
+
+}
