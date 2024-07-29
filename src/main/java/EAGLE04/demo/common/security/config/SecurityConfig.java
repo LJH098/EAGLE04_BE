43d1f51edecc202c1c678b454ff0128bdd90d665
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-29 23:14:49 +0900

    fix: SecurityConfig 수정

--- src/main/java/EAGLE04/demo/common/security/config/SecurityConfig.java
+++ src/main/java/EAGLE04/demo/common/security/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(
                 Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(
