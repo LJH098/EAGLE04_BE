9084d236ff55392a4733ce84a757620c5b9befa1
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-30 02:17:02 +0900

    fix: AuthController

--- src/main/java/EAGLE04/demo/adapter/in/auth/AuthController.java
+++ src/main/java/EAGLE04/demo/adapter/in/auth/AuthController.java
@@ -1,20 +1,12 @@
 package EAGLE04.demo.adapter.in.auth;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-
-@RestController
+@Controller
 public class AuthController {
     @GetMapping("/login")
-    public ResponseEntity<?> login() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("/oauth2/authorization/naver"));
-        return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+    public String login() {
+        return "login";
     }
 }
