c70bf541cde426dd5eecd4c106320c79b1268e2e
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-29 23:20:00 +0900

    feat: testController

---
+++ src/main/java/EAGLE04/demo/adapter/in/TestController.java
@@ -0,0 +1,12 @@
+package EAGLE04.demo.adapter.in;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
